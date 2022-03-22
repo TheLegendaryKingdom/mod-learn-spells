6a4101945920ac24edb442ccfc91a67e33831ebc
Ayahuasca <arthur.fegne@hotmail.fr>
2022-03-22 06:33:24 +0100

    Fix learning spells only if not already known

--- src/mod_learnspells.cpp
+++ src/mod_learnspells.cpp
@@ -255,7 +255,7 @@ private:
                     }
                 }
 
-                if (valid)
+                if (valid && !player->HasSpell(spellInfo->Id))
                     player->learnSpell(spellInfo->Id);
             }
         }
